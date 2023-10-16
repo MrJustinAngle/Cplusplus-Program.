cdc28fd270d9b62f8ba81aec7e63c117cfa2c141
MrJustinAngle <148124970+MrJustinAngle@users.noreply.github.com>
2023-10-16 12:37:12 -0500

    Add files via upload

---
+++ Project Two/Project Two/Source.cpp
@@ -0,0 +1,99 @@
+//Justin Angle
+//30 September 2023
+//CS 210
+//Project Two
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//Declare variables to store input
+float initInv, monDep, AnuInt, months, years;
+//Declare variables to store year end total amount, interest, and year end interest
+float totalAm, intAmt, yearTotInt;
+
+void getData()
+{
+	//Get input from user
+	cout << "********************************\n";
+	cout << "********** Data Input **********\n";
+	cout << "Initial Investment Amount: $";
+	cin >> initInv;
+	cout << "Monthly Deposit: $";
+	cin >> monDep;
+	cout << "Annual Interest: %";
+	cin >> AnuInt;
+	cout << "Number of years: ";
+	cin >> years;
+	months = years * 12;
+
+}
+
+
+
+int main()
+{
+	//Calls getData
+	getData();
+
+
+	//Set total amount to initial investment
+	totalAm = initInv;
+
+	//Display year data without monthly deposits
+	cout << "\nBalance and Interest Without Additional Monthly Deposits\n";
+	cout << "==============================================================\n";
+	cout << "Year\t\tYear End Balance\tYear End Earned Interest\n";
+	cout << "--------------------------------------------------------------\n";
+
+	//For loop to calculate investments
+	for (int i = 0; i < years; i++) {
+
+		//Calculate yearly interest
+		intAmt = (totalAm) * ((AnuInt / 100));
+
+		//Calculate year end total
+		totalAm = totalAm + intAmt;
+
+		//Print results to table
+		cout << (i + 1) << "\t\t$" << fixed << setprecision(2) << totalAm << "\t\t\t$" << intAmt << "\n";
+
+	}
+
+	//Set total amount to initial investment
+	totalAm = initInv;
+
+	//Display year data with monthly deposits
+	cout << "\n\nBalance and Interest With Additional Monthly Deposits\n";
+	cout << "==============================================================\n";
+	cout << "Year\t\tYear End Balance\tYear End Earned Interest\n";
+	cout << "--------------------------------------------------------------\n";
+
+	//For loop to calculate investments
+	for (int i = 0; i < years; i++) {
+
+		//Set yearly interest to zero at the start of the year
+		yearTotInt = 0;
+		//Another for loop to calculate interest
+		for (int j = 0; j < 12; j++) {
+
+			//Calculate monthly interest
+			intAmt = (totalAm + monDep) * ((AnuInt / 100) / 12);
+
+			//Calculate month end interest
+			yearTotInt = yearTotInt + intAmt;
+
+			//Calculate month end total
+			totalAm = totalAm + monDep + intAmt;
+
+		}
+
+		//Print results to table showcasing only two decimal points
+		cout << (i + 1) << "\t\t$" << fixed << setprecision(2) << totalAm << "\t\t\t$" << yearTotInt << "\n";
+
+
+	}
+
+	return 0;
+}
